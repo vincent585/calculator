e168d15edc4a86c46e02e5b708bd8c7bc15551c6
Vincent Klimjack <vklimjack@gmail.com>
2020-05-21 17:53:42 +0100

    calculator mostly working, to add clear functionality and decimals on next commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const add = (a, b) => a + b;
+const container = document.querySelector('#container');
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const display = document.querySelector('#calc-screen');
+const equalBtn = document.querySelector('.equals');
+
+
+
+const add = (a, b) => parseInt(a) + parseInt(b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -17,3 +26,63 @@ const operate = (a, b, operator) => {
             return 'Woops, something went wrong...';
     }
 }
+
+const calculator = {
+    displayValue: '',
+    operator: null,
+    firstOperand: '',
+    waitingForSecondOperand: false,
+}
+
+function updateDisplay(event) {
+        let digitButton = event.target.closest('.digit');
+
+        if(!digitButton) return;
+
+        if (calculator.operator && calculator.waitingForSecondOperand) {
+            calculator.firstOperand = calculator.displayValue;
+            calculator.displayValue = '';
+            calculator.waitingForSecondOperand = false;
+        } 
+
+        calculator.displayValue += digitButton.value;
+        display.textContent = calculator.displayValue;
+        console.log(calculator);
+}
+
+function getOperator(event) {
+    let operatorButton = event.target.closest('.operator');
+
+    if(!operatorButton) return;
+
+    calculator.operator = operatorButton.value;
+    calculator.waitingForSecondOperand = true;
+    console.log(calculator);
+
+} 
+
+function evaluateExpression() {
+    if (calculator.firstOperand && calculator.displayValue && calculator.operator) {
+        calculator.displayValue = parseFloat(operate(calculator.firstOperand, calculator.displayValue, calculator.operator));
+        calculator.firstOperand = calculator.displayValue;
+        display.textContent = calculator.displayValue;
+        console.log(calculator);
+    }
+}
+
+
+function handleMultipleOperators() {
+    operators.forEach(operator => {
+        operator.addEventListener('click', evaluateExpression);
+    });
+}
+
+function resetCalculator() {
+// TODO
+}
+
+handleMultipleOperators();
+equalBtn.addEventListener('click', evaluateExpression);
+container.addEventListener('click', updateDisplay);
+container.addEventListener('click', getOperator);
+clearBtn.addEventListener('click', resetCalculator);
