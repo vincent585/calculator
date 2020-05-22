990a939032aa3018d0b2c4c4d8297e8d624516ef
Vincent Klimjack <vklimjack@gmail.com>
2020-05-22 12:53:08 +0100

    fixed multiple operators doubling bug, added decimal point function and clear function

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const container = document.querySelector('#container');
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
-const clearBtn = document.querySelector('.clear');
 const display = document.querySelector('#calc-screen');
 const equalBtn = document.querySelector('.equals');
 
 
 
-const add = (a, b) => parseInt(a) + parseInt(b);
+const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -28,7 +27,7 @@ const operate = (a, b, operator) => {
 }
 
 const calculator = {
-    displayValue: '',
+    displayValue: 0,
     operator: null,
     firstOperand: '',
     waitingForSecondOperand: false,
@@ -41,13 +40,20 @@ function updateDisplay(event) {
 
         if (calculator.operator && calculator.waitingForSecondOperand) {
             calculator.firstOperand = calculator.displayValue;
-            calculator.displayValue = '';
+            calculator.displayValue = 0;
             calculator.waitingForSecondOperand = false;
         } 
 
-        calculator.displayValue += digitButton.value;
-        display.textContent = calculator.displayValue;
-        console.log(calculator);
+        if (calculator.displayValue === 0) {
+            calculator.displayValue = digitButton.value;
+            display.textContent = calculator.displayValue;
+        } else {
+            calculator.displayValue += digitButton.value;
+            display.textContent = calculator.displayValue;
+            
+        }
+        console.log('update display', calculator);
+        
 }
 
 function getOperator(event) {
@@ -56,20 +62,36 @@ function getOperator(event) {
     if(!operatorButton) return;
 
     calculator.operator = operatorButton.value;
-    calculator.waitingForSecondOperand = true;
-    console.log(calculator);
+    calculator.waitingForSecondOperand = true; 
+    console.log('get operator', calculator);
 
 } 
 
-function evaluateExpression() {
-    if (calculator.firstOperand && calculator.displayValue && calculator.operator) {
-        calculator.displayValue = parseFloat(operate(calculator.firstOperand, calculator.displayValue, calculator.operator));
-        calculator.firstOperand = calculator.displayValue;
-        display.textContent = calculator.displayValue;
-        console.log(calculator);
-    }
-}
+function evaluateExpression(event) {
 
+    let operatorButton = event.target.closest('.operator');
+    let equalBtn = event.target.closest('.equals');
+
+    if (equalBtn) {
+        if (calculator.firstOperand && calculator.displayValue && calculator.operator) {
+            calculator.displayValue = parseFloat(operate(calculator.firstOperand, calculator.displayValue, calculator.operator));
+            calculator.firstOperand = calculator.displayValue;
+            display.textContent = calculator.displayValue;
+            calculator.operator = null;
+            console.log('eval expression', calculator);
+        }
+    }
+
+    if (operatorButton) {
+        if (calculator.firstOperand && calculator.displayValue && calculator.operator) {
+            calculator.displayValue = parseFloat(operate(calculator.firstOperand, calculator.displayValue, calculator.operator));
+            calculator.firstOperand = calculator.displayValue;
+            display.textContent = calculator.displayValue;
+        }
+    }
+
+    
+}
 
 function handleMultipleOperators() {
     operators.forEach(operator => {
@@ -77,12 +99,34 @@ function handleMultipleOperators() {
     });
 }
 
-function resetCalculator() {
-// TODO
+function inputDecimal(event) {
+    let decimalBtn = event.target.closest('.decimal');
+
+    if (!decimalBtn) return;
+
+    if (display.textContent.includes('.')) {
+        return;
+    }
+
+    calculator.displayValue += '.';
+    display.textContent = calculator.displayValue;
+}
+
+function resetCalculator(event) {
+    let clearBtn = event.target.closest('.clear');
+
+    if (!clearBtn) return;
+
+    calculator.displayValue = 0;
+    calculator.firstOperand = '';
+    calculator.operator = null;
+    calculator.waitingForSecondOperand = false;
+    display.textContent = calculator.displayValue;
 }
 
 handleMultipleOperators();
 equalBtn.addEventListener('click', evaluateExpression);
 container.addEventListener('click', updateDisplay);
 container.addEventListener('click', getOperator);
-clearBtn.addEventListener('click', resetCalculator);
+container.addEventListener('click', resetCalculator);
+container.addEventListener('click', inputDecimal);
